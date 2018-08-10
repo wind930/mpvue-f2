155f7a0b6b4341f802065c5770b2309be0ea4641
soonfy <soonfy@163.com>
2018-08-10 17:42:10 +0800

    wxapp demo init

--- src/pages/wx-echarts/index.vue
+++ src/pages/wx-echarts/index.vue
@@ -1,27 +1,232 @@
 <template>
   <div class="page">
-    <a v-for="(chart,key) in charts" :key="key" class="list-item" :href="'pages/'+chart.name+'/main'">
-      <image :src="'../../static/img/'+chart.name+'.jpg'" :data-page="chart.name" mode="aspectFit" />
-      {{chart.value}}
-    </a>
+    <div class="chart">
+      <div class="title">
+        <span>echarts 图表</span>
+      </div>
+      <div class="chart-wrapper">
+        <mpvue-echarts :echarts="echarts" :onInit="initSingle" canvasId="single-canvas" ref="single" />
+      </div>
+    </div>
+    <div class="lazy">
+      <div class="title">
+        <span>延迟加载</span>
+      </div>
+      <div class="chart-wrapper">
+        <span v-if="status" @click="lazyLoadChart">点击此处加载图形</span>
+        <mpvue-echarts v-else :echarts="echarts" :onInit="initLazy" canvasId="lazy-canvas" ref="lazy" lazyLoad />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 
+import * as echarts from 'echarts'
+import mpvueEcharts from 'mpvue-echarts'
+
 export default {
   data () {
     return {
-      charts: [
-        { name: 'wx-echarts', value: 'echarts' },
-        { name: 'wx-f2', value: 'f2' }
-      ]
+      echarts: echarts,
+      initSingle: this.drawSingle,
+      initLazy: this.drawLazy,
+      status: true
     }
   },
 
-  components: {},
+  components: {
+    mpvueEcharts
+  },
 
-  methods: {},
+  methods: {
+    drawSingle (canvas, width, height) {
+      const singleChart = echarts.init(canvas, null, {
+        width: width,
+        height: height
+      })
+      canvas.setChart(singleChart)
+
+      var option = {
+        title: {
+          text: '测试下面legend的红色区域不应被裁剪',
+          left: 'center'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            // 坐标轴指示器，坐标轴触发有效
+            type: 'shadow'
+            // 默认为直线，可选为：'line' | 'shadow'
+          }
+        },
+        color: ['#37A2DA', '#67E0E3', '#9FE6B8'],
+        legend: {
+          data: ['A', 'B', 'C'],
+          top: 50,
+          left: 'center',
+          backgroundColor: 'red',
+          z: 100
+        },
+        grid: {
+          containLabel: true
+        },
+
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+          // show: false
+        },
+        yAxis: {
+          x: 'center',
+          type: 'value',
+          splitLine: {
+            lineStyle: {
+              type: 'dashed'
+            }
+          }
+          // show: false
+        },
+        series: [{
+          name: 'A',
+          type: 'line',
+          smooth: true,
+          data: [18, 36, 65, 30, 78, 40, 33]
+        }, {
+          name: 'B',
+          type: 'line',
+          smooth: true,
+          data: [12, 50, 51, 35, 70, 30, 20]
+        }, {
+          name: 'C',
+          type: 'line',
+          smooth: true,
+          data: [10, 30, 31, 50, 40, 20, 10]
+        }]
+      }
+
+      singleChart.setOption(option)
+      return singleChart
+    },
+    drawLazy (canvas, width, height) {
+      const lazyChart = echarts.init(canvas, null, {
+        width: width,
+        height: height
+      })
+      canvas.setChart(lazyChart)
+
+      var option = {
+        color: ['#37a2da', '#32c5e9', '#67e0e3'],
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            // 坐标轴指示器，坐标轴触发有效
+            type: 'shadow'
+            // 默认为直线，可选为：'line' | 'shadow'
+          }
+        },
+        legend: {
+          data: ['热度', '正面', '负面']
+        },
+        grid: {
+          left: 20,
+          right: 20,
+          bottom: 15,
+          top: 40,
+          containLabel: true
+        },
+        xAxis: [
+          {
+            type: 'value',
+            axisLine: {
+              lineStyle: {
+                color: '#999'
+              }
+            },
+            axisLabel: {
+              color: '#666'
+            }
+          }
+        ],
+        yAxis: [
+          {
+            type: 'category',
+            axisTick: { show: false },
+            data: ['汽车之家', '今日头条', '百度贴吧', '一点资讯', '微信', '微博', '知乎'],
+            axisLine: {
+              lineStyle: {
+                color: '#999'
+              }
+            },
+            axisLabel: {
+              color: '#666'
+            }
+          }
+        ],
+        series: [
+          {
+            name: '热度',
+            type: 'bar',
+            label: {
+              normal: {
+                show: true,
+                position: 'inside'
+              }
+            },
+            data: [300, 270, 340, 344, 300, 320, 310],
+            itemStyle: {
+              // emphasis: {
+              //   color: '#37a2da'
+              // }
+            }
+          },
+          {
+            name: '正面',
+            type: 'bar',
+            stack: '总量',
+            label: {
+              normal: {
+                show: true
+              }
+            },
+            data: [120, 102, 141, 174, 190, 250, 220],
+            itemStyle: {
+              // emphasis: {
+              //   color: '#32c5e9'
+              // }
+            }
+          },
+          {
+            name: '负面',
+            type: 'bar',
+            stack: '总量',
+            label: {
+              normal: {
+                show: true,
+                position: 'left'
+              }
+            },
+            data: [-20, -32, -21, -34, -90, -130, -110],
+            itemStyle: {
+              // emphasis: {
+              //   color: '#67e0e3'
+              // }
+            }
+          }
+        ]
+      }
+
+      lazyChart.setOption(option)
+      return lazyChart
+    },
+    lazyLoadChart () {
+      this.status = false
+      setTimeout(() => {
+        this.$refs.lazy.init()
+      }, 0)
+    }
+  },
 
   created () {}
 }
@@ -32,39 +237,32 @@ export default {
 .page {
   padding: 10px;
   margin: 0;
-  list-style: none;
-
-  display: -webkit-box;
-  display: -moz-box;
-  display: -ms-flexbox;
-  display: -webkit-flex;
-  display: flex;
-
-  -webkit-flex-flow: row wrap;
-  justify-content: space-between;
-  font-family: -apple-system-font, "Helvetica Neue", sans-serif;
+  font-size: 16px;
+  color: #999;
+  font-family: -apple-system-font, 'Helvetica Neue', sans-serif;
 }
 
-.list-item {
-  padding: 5px;
-  width: 100px;
-  height: 150px;
-  text-align: center;
-  font-size: 12px;
-  margin-top: 10px;
+.page div {
+  margin-bottom: 20px;
+  padding: 10px 0;
 }
 
-.list-item image {
-  box-sizing: border-box;
+.title > span {
+  display: inline-block;
+  padding: 0 10px;
+  border-left: #448ef6 4px solid;
+}
+
+.chart-wrapper {
   width: 100%;
-  height: 100px;
-  padding: 12px;
-  background-color: #fff;
-  border: 1px solid #eee;
-  border-radius: 4px;
-  display: block;
-  margin: 0 auto;
-  margin-bottom: 10px;
+  height: 300px;
+}
+
+.chart-wrapper > span {
+  display: inline-block;
+  width: 100%;
+  height: 80%;
+  text-align: center;
 }
 
 </style>
--- src/pages/wx-f2/index.vue
+++ src/pages/wx-f2/index.vue
@@ -1,27 +1,170 @@
 <template>
   <div class="page">
-    <a v-for="(chart,key) in charts" :key="key" class="list-item" :href="'pages/'+chart.name+'/main'">
-      <image :src="'../../static/img/'+chart.name+'.jpg'" :data-page="chart.name" mode="aspectFit" />
-      {{chart.value}}
-    </a>
+    <div class="chart">
+      <div class="title">
+        <span>f2 图表</span>
+      </div>
+      <div class="chart-wrapper">
+        <mpvue-f2 :f2="f2" :onInit="initSingle" canvasId="single-canvas" ref="single" />
+      </div>
+    </div>
+    <div class="lazy">
+      <div class="title">
+        <span>延迟加载</span>
+      </div>
+      <div class="chart-wrapper">
+        <span v-if="status" @click="lazyLoadChart">点击此处加载图形</span>
+        <mpvue-f2 v-else :f2="f2" :onInit="initLazy" canvasId="lazy-canvas" ref="lazy" lazyLoad />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 
+import mpvueF2 from 'mpvue-f2'
+const F2 = require('@antv/f2')
+
 export default {
   data () {
     return {
-      charts: [
-        { name: 'wx-echarts', value: 'echarts' },
-        { name: 'wx-f2', value: 'f2' }
-      ]
+      f2: F2,
+      initSingle: this.drawSingle,
+      initLazy: this.drawLazy,
+      status: true
     }
   },
 
-  components: {},
+  components: {
+    mpvueF2
+  },
 
-  methods: {},
+  methods: {
+    drawSingle (canvas, width, height) {
+      let singleChart = new F2.Chart({
+        el: canvas,
+        width,
+        height
+      })
+
+      const data = [{ 'year': 1997, 'type': 'United States', 'value': 4.9 }, { 'year': 1997, 'type': 'Florida', 'value': 4.8 }, { 'year': 1998, 'type': 'United States', 'value': 4.5 }, { 'year': 1998, 'type': 'Florida', 'value': 4.3 }, { 'year': 1999, 'type': 'United States', 'value': 4.2 }, { 'year': 1999, 'type': 'Florida', 'value': 3.9 }, { 'year': 2000, 'type': 'United States', 'value': 4 }, { 'year': 2000, 'type': 'Florida', 'value': 3.7 }, { 'year': 2001, 'type': 'United States', 'value': 4.7 }, { 'year': 2001, 'type': 'Florida', 'value': 4.7 }, { 'year': 2002, 'type': 'United States', 'value': 5.8 }, { 'year': 2002, 'type': 'Florida', 'value': 5.6 }, { 'year': 2003, 'type': 'United States', 'value': 6 }, { 'year': 2003, 'type': 'Florida', 'value': 5.2 }, { 'year': 2004, 'type': 'United States', 'value': 5.5 }, { 'year': 2004, 'type': 'Florida', 'value': 4.6 }, { 'year': 2005, 'type': 'United States', 'value': 5.1 }, { 'year': 2005, 'type': 'Florida', 'value': 3.7 }, { 'year': 2006, 'type': 'United States', 'value': 4.6 }, { 'year': 2006, 'type': 'Florida', 'value': 3.2 }, { 'year': 2007, 'type': 'United States', 'value': 4.6 }, { 'year': 2007, 'type': 'Florida', 'value': 4 }, { 'year': 2008, 'type': 'United States', 'value': 5.8 }, { 'year': 2008, 'type': 'Florida', 'value': 6.3 }, { 'year': 2009, 'type': 'United States', 'value': 9.3 }, { 'year': 2009, 'type': 'Florida', 'value': 10.4 }, { 'year': 2010, 'type': 'United States', 'value': 9.6 }, { 'year': 2010, 'type': 'Florida', 'value': 11.1 }, { 'year': 2011, 'type': 'United States', 'value': 8.9 }, { 'year': 2011, 'type': 'Florida', 'value': 10 }, { 'year': 2012, 'type': 'United States', 'value': 8.1 }, { 'year': 2012, 'type': 'Florida', 'value': 8.5 }, { 'year': 2013, 'type': 'United States', 'value': 7.4 }, { 'year': 2013, 'type': 'Florida', 'value': 7.2 }, { 'year': 2014, 'type': 'United States', 'value': 6.2 }, { 'year': 2014, 'type': 'Florida', 'value': 6.3 }, { 'year': 2015, 'type': 'United States', 'value': 5.3 }, { 'year': 2015, 'type': 'Florida', 'value': 5.4 }, { 'year': 2016, 'type': 'United States', 'value': 4.9 }, { 'year': 2016, 'type': 'Florida', 'value': 4.9 }, { 'year': 2017, 'type': 'United States', 'value': 4.4 }, { 'year': 2017, 'type': 'Florida', 'value': 4.3 }]
+
+      singleChart.source(data, {
+        year: {
+          range: [0, 1],
+          ticks: [1997, 1999, 2001, 2003, 2005, 2007, 2009, 2011, 2013, 2015, 2017]
+        },
+        value: {
+          tickCount: 10,
+          formatter (val) {
+            return val.toFixed(1) + '%'
+          }
+        }
+      })
+
+      singleChart.tooltip({
+        custom: true,
+        showCrosshairs: true,
+        onChange (obj) {
+          const legend = singleChart.get('legendController').legends.top[0]
+          const tooltipItems = obj.items
+          const legendItems = legend.items
+          const map = {}
+          legendItems.map(item => {
+            map[item.name] = Object.assign({}, item)
+          })
+          tooltipItems.map(item => {
+            const { name, value } = item
+            if (map[name]) {
+              map[name].value = value
+            }
+          })
+          legend.setItems(Object.values(map))
+        },
+        onHide () {
+          const legend = singleChart.get('legendController').legends.top[0]
+          legend.setItems(singleChart.getLegendItems().country)
+        }
+      })
+
+      singleChart.guide().rect({
+        start: [2011, 'max'],
+        end: ['max', 'min'],
+        style: {
+          fill: '#CCD6EC',
+          opacity: 0.3
+        }
+      })
+      singleChart.guide().text({
+        position: [2014, 'max'],
+        content: 'Scott administratio\n(2011 to present)',
+        style: {
+          fontSize: 10,
+          textBaseline: 'top'
+        }
+      })
+
+      singleChart.line().position('year*value').color('type', val => {
+        if (val === 'United States') {
+          return '#ccc'
+        }
+      })
+      singleChart.render()
+      return singleChart
+    },
+    drawLazy (canvas, width, height) {
+      let lazyChart = new F2.Chart({
+        el: canvas,
+        width,
+        height
+      })
+
+      var Global = F2.Global
+      var data = [
+        { country: '巴西', population: 18203 },
+        { country: '印尼', population: 23489 },
+        { country: '美国', population: 29034 },
+        { country: '印度', population: 104970 },
+        { country: '中国', population: 131744 }
+      ]
+
+      lazyChart.source(data, {
+        population: {
+          tickCount: 5
+        }
+      })
+      lazyChart.coord({
+        transposed: true
+      })
+      lazyChart.axis('country', {
+        line: Global._defaultAxis.line,
+        grid: null
+      })
+      lazyChart.axis('population', {
+        line: null,
+        grid: Global._defaultAxis.grid,
+        label: function label (text, index, total) {
+          var textCfg = {}
+          if (index === 0) {
+            textCfg.textAlign = 'left'
+          } else if (index === total - 1) {
+            textCfg.textAlign = 'right'
+          }
+          return textCfg
+        }
+      })
+      lazyChart.interval().position('country*population')
+      lazyChart.render()
+
+      return lazyChart
+    },
+    lazyLoadChart () {
+      this.status = false
+      setTimeout(() => {
+        this.$refs.lazy.init()
+      }, 0)
+    }
+  },
 
   created () {}
 }
@@ -32,39 +175,32 @@ export default {
 .page {
   padding: 10px;
   margin: 0;
-  list-style: none;
-
-  display: -webkit-box;
-  display: -moz-box;
-  display: -ms-flexbox;
-  display: -webkit-flex;
-  display: flex;
-
-  -webkit-flex-flow: row wrap;
-  justify-content: space-between;
+  font-size: 16px;
+  color: #999;
   font-family: -apple-system-font, "Helvetica Neue", sans-serif;
 }
 
-.list-item {
-  padding: 5px;
-  width: 100px;
-  height: 150px;
-  text-align: center;
-  font-size: 12px;
-  margin-top: 10px;
+.page div {
+  margin-bottom: 20px;
+  padding: 10px 0;
 }
 
-.list-item image {
-  box-sizing: border-box;
+.title > span {
+  display: inline-block;
+  padding: 0 10px;
+  border-left: #448ef6 4px solid;
+}
+
+.chart-wrapper {
   width: 100%;
-  height: 100px;
-  padding: 12px;
-  background-color: #fff;
-  border: 1px solid #eee;
-  border-radius: 4px;
-  display: block;
-  margin: 0 auto;
-  margin-bottom: 10px;
+  height: 300px;
+}
+
+.chart-wrapper > span {
+  display: inline-block;
+  width: 100%;
+  height: 80%;
+  text-align: center;
 }
 
 </style>
